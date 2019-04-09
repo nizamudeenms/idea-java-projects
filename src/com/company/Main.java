a360036540b50003bad895c9eaca29de82ada983
Nizam <nizamudeenms@gmail.com>
2019-04-09 23:21:57 +0530

    Extra Long Factorials.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,52 +1,35 @@
 package com.company;
 
+import java.math.BigInteger;
 import java.util.Scanner;
 
 public class Main {
 
 
+
+
+
     private static final Scanner scanner = new Scanner(System.in);
 
-
     public static void main(String[] args) {
-        // write your code here
-        System.out.println("Begin");
-
-        int t = scanner.nextInt();
+        int n = scanner.nextInt();
         scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
 
-        for (int tItr = 0; tItr < t; tItr++) {
-            int n = scanner.nextInt();
-            scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
-
-            int result = findDigits(n);
-
-            System.out.println("result = " + result);
-
-        }
-
+        extraLongFactorials(n);
 
         scanner.close();
     }
 
-    static int findDigits(int n) {
+    static void extraLongFactorials(int n) {
+        BigInteger bigI = new BigInteger("1");
+        BigInteger bigT = new BigInteger("0");
 
-        int divs = 0;
-
-
-
-        int[] digit = String.valueOf(n).chars().map(Character::getNumericValue).toArray();
-
-        for (int i = 0; i < digit.length; i++) {
-            if (digit[i] != 0) {
-                System.out.println("n+\" \"+digit[i]+\" \"+n%digit[i] = " + n+" "+digit[i]+" "+n%digit[i]);
-                if ((n % digit[i]) == 0) {
-                    divs++;
-                }
-            }
+        for (int i = 1; i <=n ; i++) {
+            bigT = BigInteger.valueOf(i);
+            bigI = bigI.multiply(bigT);
         }
-        return divs;
-    }
+        System.out.println("bigI = " + bigI);
 
+    }
 
 }
